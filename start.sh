13ec2f2399756514562e7a14bb8bb30664114b0e
xovoefx <98139422+xovoefx@users.noreply.github.com>
2022-11-21 21:53:19 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,23 @@
+if [ -z $UPSTREAM_REPO ]
+
+then
+
+  echo "Cloning main Repository"
+
+  git clone https://github.com/xovoefx/DEVIL-V3 /jack ma
+
+else
+
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+
+  git clone $UPSTREAM_REPO /EvaMaria
+
+fi
+
+cd /Jack Ma
+
+pip3 install -U -r requirements.txt
+
+echo "Starting Bot...."
+
+python3 bot.py
